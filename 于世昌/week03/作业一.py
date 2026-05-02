99736c8a483e5adf63986f94dfd74ca269d46c56
于世昌 <yushichang@chinadaas.com>
2026-05-02 15:44:01 +0800

    三周作业修改

--- 于世昌/week03/作业一.py
+++ 于世昌/week03/作业一.py
@@ -14,7 +14,7 @@
 先准备100个这种数据，
 然后通过词表 对数据集进行预处理，长度约定为5，小于5的补padding
 3、创建embedding， 词表大小 = 2的词表长度， 维度先用64
-4、创建rnn 维度128， hidden_size 64 ， num_layers 
+4、创建rnn 维度128， hidden_size 64 ， num_layers
 5、rnn走完走池化
 6、池化成1维向量后，走linear输出一个 预测的分类结果
 """
@@ -34,20 +34,23 @@ class MyChineseClassificationModule(torch.nn.Module):
         super(MyChineseClassificationModule, self).__init__()
         dim = 64
         self.emb = nn.Embedding(num_embeddings=vocabulary_size, embedding_dim=dim)
-        self.rnn = nn.LSTM(input_size=dim, hidden_size=64, num_layers=1, batch_first=True)
+        self.rnn = nn.RNN(input_size=dim, hidden_size=64, num_layers=1, batch_first=True)
+        # self.pool = nn.AdaptiveAvgPool2d((1, 64))
         self.linear = nn.Linear(64, 5)
-    
+
     def forward(self, input):
-         emb_output = self.emb(input)
-         rnn_output, _ = self.rnn(emb_output)
-         flat = torch.mean(rnn_output, dim=1) 
-         return self.linear(flat)
-        
+         emb_output = self.emb(input) # batch, 5, 64
+         rnn_output, _ = self.rnn(emb_output) # batch, 5, 64
+        #  pool_output = self.pool(rnn_output)
+        #  flat = pool_output.squeeze(1)
+         rnn_last_step_output = rnn_output[:, -1, :] # batch, 64
+         return self.linear(rnn_last_step_output)
+
 def prepare_vocabulary():
     """准备词表
     """
     # 句子中包含 "你"
-    chinese_sentence = "清晨的巷弄飘着豆浆香，你蹲在墙角喂流浪猫。小猫怯生生地靠近，轻轻蹭了蹭你的指尖。风掠过树梢，落下几片黄叶。你笑着起身，摸了摸它的头，轻声说下次再来看它。转身时，阳光刚好落在肩头，暖得像一句温柔的约定。平凡的小美好，总在你不经意的瞬间，悄悄落在身旁。"
+    chinese_sentence = "我一人口手足耳目头发心肝脾肺肾胃肠皮肉血骨筋天地日月星云风雨雷电山水火木金土石田河海江湖波浪泉溪雾霜雪冰春夏秋冬昼夜早晚晨昏东西南北中前后左右上下内外高低长短大小多少远近深浅宽窄厚薄黑白红黄蓝绿青紫灰粉一二三四五六七八九十百千万亿零两半整我你他她它们咱吾尔彼这那此谁何啥每各某的地得之于在向往至到从自由沿顺逆随伴同共和与及或且并但却虽然因为所以故则就才又再很更也已必刚正便即都只就别没去来过回出进开开关闭起落坐立行走跑跳吃喝穿住用看听说读写想记念思考感觉爱恨喜怒哀惧好恶欲望心意神情态度言行举止家国城乡街巷楼房屋院门窗桌椅床柜灯镜纸笔书本字画歌舞影戏琴棋书画车船飞机枪炮钱票券卡软硬轻重快慢冷热干湿亮暗动静强弱好坏优劣美丑真假虚实有无是非对错是"
     vocabulary_list = ["padding", "unknown"] + list(set(chinese_sentence))
     vocabulary_dict = {word:index for index, word in enumerate(vocabulary_list)}
     return vocabulary_dict
@@ -64,8 +67,8 @@ def prepare_dataset(file_name):
     # 拼接csv文件路径
     csv_path = os.path.join(current_dir, file_name)
     df = pd.read_csv(csv_path, sep=",")
-    return list(df["sentence"]), [torch.argmax(torch.LongTensor(json.loads(item))) for item in df["vector"]]
-    
+    return list(df["sentence"]), [json.loads(item) for item in df["vector"]]
+
 def deal_dataset(data_list, vocabulary_dict):
     """处理数据集
     """
@@ -84,7 +87,7 @@ def deal_dataset(data_list, vocabulary_dict):
         # 将处理后的数据集放到最终交给模型训练的data_list_deal中
         data_list_deal.append(data_vector[:5])
     return data_list_deal
-        
+
 def test_model(model):
     # 测试集的也需要用词表这一套
     # 词表
@@ -107,21 +110,21 @@ def test_model(model):
         # 循环测试集
         for batch_x, batch_y in test_dataLoader:
             # 前向预测
-            outputs = model(batch_x)  
-            
+            outputs = model(batch_x)
+
             # 3. 批量取预测类别（取最大概率的下标）[4,4,4,4,4,4,4,4,4,4]
             predict_labels = torch.argmax(outputs, dim=1)
 
             # 4. 统计正确个数
-            # 
-            total_correct += (predict_labels == batch_y).sum().item()
+            #
+            total_correct += (predict_labels == torch.argmax(batch_y, dim=1)).sum().item()
             total_num += len(batch_y)
 
         # 5. 计算准确率
         acc = total_correct / total_num
         print(f"测试集准确率：{acc:.4f}")
         return acc
-    
+
 def main():
     # print(f"词表长度：{len(prepare_vocabulary())}")
     # 词表
@@ -132,34 +135,33 @@ def main():
     # 通过词表，处理数据集，以5维向量为基本单元，少的补padding
     data_list_deal = deal_dataset(data_list, vocabulary_dict)
     # print(f"处理后的数据集为:{data_list_deal}")
-    
+
     # 将数据集 和 标注 转批量向量
     tensor_data_set = TensorDataset(torch.LongTensor(data_list_deal), torch.LongTensor(data_label_list))
     dataLoader = DataLoader(tensor_data_set, batch_size=5, shuffle=True)
-    
+
     # 创建模型
     myChineseClassificationModule = MyChineseClassificationModule(len(vocabulary_dict))
     # 分类任务损失函数用交叉熵
     loss_function = nn.CrossEntropyLoss()
     # 优化器用adam
-    optimizer = torch.optim.Adam(myChineseClassificationModule.parameters(), lr=0.0005)
-    max_epoch = 1500
-    myChineseClassificationModule.train()
+    optimizer = torch.optim.Adam(myChineseClassificationModule.parameters(), lr=0.001)
+    max_epoch = 500
     for epoch in range(max_epoch):
         for data_batch, label_batch in dataLoader:
             # 清一下步长
             optimizer.zero_grad()
             y_pred_batch = myChineseClassificationModule(data_batch)
-            loss = loss_function(y_pred_batch, label_batch)
+            loss = loss_function(y_pred_batch, torch.argmax(label_batch, dim=1))
             loss.backward()
             optimizer.step()
-    
-    # 测试模型    
-    # result = myChineseClassificationModule(torch.LongTensor([51, 0, 0, 0, 0]))
-    # print(result)
+
+    # 测试模型
+    result = myChineseClassificationModule(torch.LongTensor([[51, 0, 0, 0, 0]]))
+    print(result)
     test_model(myChineseClassificationModule)
-    
-    
+
+
 
 if __name__ == "__main__":
     main()
